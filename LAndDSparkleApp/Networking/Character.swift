6bbc2a9d9c842faf4b1aae50176a9650cee0f942
tesddev <tesleem.amuda@gmail.com>
2024-07-02 21:45:16 +0100

    add file and update Character model

--- LAndDSparkleApp/Networking/Character.swift
+++ LAndDSparkleApp/Networking/Character.swift
@@ -7,17 +7,6 @@
 
 import Foundation
 
-//struct Character: Codable, Identifiable {
-//    let id: Int?
-//    let name: String?
-//    let status: String?
-//    let species: String?
-//    let image: URL?
-//    static let empty = Character(id: 0, name: "Tes", status: "Popular", species: "Human", image: URL(string: "https://avatars.githubusercontent.com/u/58847828?v=4"))
-//}
-
-import Foundation
-
 // MARK: - Character
 struct Character: Codable, Identifiable {
     var id: Int?
