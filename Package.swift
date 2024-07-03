3da9df158283e1bb2b0b8f6cd6f4ef4aa68e51ff
tesddev <tesleem.amuda@gmail.com>
2024-07-03 23:53:17 +0100

    add package.swift file

--- Package.swift
+++ Package.swift
@@ -5,4 +5,25 @@
 //  Created by GIGL-MAC on 03/07/2024.
 //
 
-import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "LAndDSparkleApp",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "LAndDSparkleApp",
+            targets: ["LAndDSparkleApp"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.9.3")
+    ],
+    targets: [
+        .target(
+            name: "LAndDSparkleApp",
+            dependencies: ["ViewInspector"],
+            path: "Sources")
+    ]
+)
